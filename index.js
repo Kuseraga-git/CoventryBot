4b0b2afe6f0887a2f0b4b127ae06de0ffd911e74
ACANTELOUP <tony.canteloup@gmail.com>
2021-11-21 20:10:42 +0100

    heroku fait chier

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-//const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-//client.login(config.BOT_TOKEN);
+//const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+//client.login(config.TOKEN);
 client.login(process.env.TOKEN);
 /** 
  * Import other file
