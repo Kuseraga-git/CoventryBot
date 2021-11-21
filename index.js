1c6db828c266e148ea41fbc87a148b9e7b425ebb
ACANTELOUP <tony.canteloup@gmail.com>
2021-11-21 19:47:34 +0100

    heroku

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-client.login(config.BOT_TOKEN);
+//client.login(config.BOT_TOKEN);
+client.login(process.env.TOKEN);
 /** 
  * Import other file
  */
