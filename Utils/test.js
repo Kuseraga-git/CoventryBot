98d69dcf17f96710c730e58130f4de432d7db4aa
ACANTELOUP <tony.canteloup@gmail.com>
2021-11-21 20:59:23 +0100

    testing

--- Utils/test.js
+++ Utils/test.js
@@ -3,7 +3,7 @@ const blackList = require("./../Tupper_blacklist.json");
 function test(msg){
     if (msg.channelId == "884843344117788762") {
         if (msg.author.username in blackList) {
-            msg.channel.send(msg.author.username)
+            msg.delete()
         }
     }
 }
