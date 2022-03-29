5bae78c835fd2d1a07450635627ee4382d4bced1
ACANTELOUP <anthony.canteloup@ynov.com>
2022-03-29 17:41:47 +0200

    Change role creation logic

--- Utils/Role.js
+++ Utils/Role.js
@@ -8,10 +8,44 @@ const roleManager = require("./../Role_Manager.json")
 const reservation = require("./../Reservation.json")
 const aliases = require("./../Aliases.json")
 
-function attributeRoles(member, msg, paramArr){
-    member.roles.add(msg.guild.roles.cache.find(r => r.id === roleManager.Nb_Player[paramArr[1].trim()]).id);
-    member.roles.add(msg.guild.roles.cache.find(r => r.id === roleManager.Classe[paramArr[1].trim()][paramArr[4].trim()]).id);
-    member.roles.add(msg.guild.roles.cache.find(r => r.id === roleManager.Species[paramArr[2].trim()][parseInt(paramArr[1].trim())]).id);
+function attributeRoles(member, msg, number, socialClass, species){
+    member.roles.add(msg.guild.roles.cache.find(r => r.id === roleManager.Nb_Player[number]).id);
+    member.roles.add(msg.guild.roles.cache.find(r => r.id === roleManager.Classe[number][socialClass]).id);
+    member.roles.add(msg.guild.roles.cache.find(r => r.id === roleManager.Species[species][Object.keys(roleManager.Nb_Player).indexOf(number) +1]).id);
 }
 
+const roleList = [
+    '<:Animorphe:958383605736960000>',
+    '<:Dragon:958383697671913603>',
+    '<:Elfe:958383733004722207>',
+    '<:Fee:958383756593471558>',
+    '<:Kistune:958383810813259806>',
+    '<:Legende:958383841289076846>',
+    '<:Lycan:958383954384289802>',
+    '<:Necromantien:958383998227341312>',
+    '<:Sirene:958384036378738738>',
+    '<:Sorcier:958384056532336720>',
+    '<:Valarjar:958384093442236478>',
+    '<:Vampire:958384120952651837>',
+    '<:Humain:958383780186427392>'
+]
+
+const characterNumber = [
+    '1️⃣',
+    '2️⃣',
+    '3️⃣',
+    '4️⃣',
+    '5️⃣',
+    '6️⃣'
+]
+
+const socialClasses = [
+    '💵',
+    '🧽',
+    '🥻'
+]
+
+exports.roleList = roleList
+exports.characterNumber = characterNumber
+exports.socialClasses = socialClasses
 exports.attributeRoles = attributeRoles
--- index.js
+++ index.js
@@ -39,33 +39,98 @@ client.on("message", msg => {
      *  Flag Manager
      */
     // Use cmd to create Role + add it
-    if (Utils.hasPrefix(msg) && Utils.hasRights(msg.member._roles)){
-        const paramArr = msg.content.split(',').map(str => str.trim())
-        if (msg.guild.roles.cache.find(role => role.name === paramArr[0].replace('!cov ', '')) == undefined)
-        {
-            let rInfo = msg.guild.roles.cache.find(r => r.id === roleManager.Nb_Player[paramArr[1].trim()])
-            let member = msg.mentions.members.first()
-            msg.guild.roles.create({
-                name: paramArr[0].replace('!cov ', ''),
-                color: roleManager.Species[paramArr[2].trim()][0],
-                position: rInfo.rawPosition - 1,
-                mentionable : true,
-                reason: 'we needed a role for Super Cool People',
-            })
-            .then(role => {
-                member.roles.add(role.id);
-            })
-            .catch(console.error);
-            Role.attributeRoles(member, msg, paramArr)
-            const exampleEmbed = new MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle('Role crée !')
-                .setDescription("J'ai crée et attribué les rôles du joueur o/")
-                .setTimestamp();
+    if (Utils.hasPrefix(msg) && Utils.hasRights(msg.member._roles) && msg.content.split(',').length == 2){
+        
+        const member = msg.mentions.members.first() // Get mentionned user
+        const roleName = msg.content.match(/,(.*)$/g)[0].substring(1).trim()
 
-            msg.channel.send({ embeds: [exampleEmbed] });
+        const channel = client.channels.cache.get(msg.channelId)
+        let embedMessage = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Création du rôle')
+            .setDescription(`Quelle est la race de ${roleName}`)
+            .setTimestamp();
+
+        channel.send({ embeds: [embedMessage] }).then(message => {
+            for (const role of Role.roleList)
+                message.react(role)
+            const filter = (reaction, user) => {
+                return Role.roleList.includes('<:' + reaction.emoji.name + ':' + reaction.emoji.id + '>') && Utils.hasRights(reaction.message.guild.members.cache.get(user.id).roles.member._roles)
+            }
+            message.awaitReactions({ filter, max: 1, time: 60000, errors: ['time'] })
+                .then(collected => {
+                    const reaction = collected.first()
+                    const species = reaction.emoji.name
+                    message.reactions.removeAll()
+	                    .catch(error => console.error('Failed to clear reactions:', error));
+                    embedMessage = new MessageEmbed()
+                        .setDescription(`Un nouveau ${species} ! \nCe serait le personnage n°`)
+                    message.edit({embeds: [embedMessage]})
+                    for (const number of Role.characterNumber)
+                        message.react(number)
+                    const filter = (reaction, user) => {
+                        return ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣'].includes(reaction.emoji.name) && Utils.hasRights(reaction.message.guild.members.cache.get(user.id).roles.member._roles);
+                    }
+                    message.awaitReactions({ filter, max: 1, time: 60000, errors: ['time'] })
+                        .then(collected => {
+                            const reaction = collected.first()
+                            const number = reaction.emoji.name
+                            message.reactions.removeAll()
+	                            .catch(error => console.error('Failed to clear reactions:', error));
+                            embedMessage = new MessageEmbed()
+                                .setDescription(`Un nouveau ${species} ! \nPersonnage n°${number}\nClasse sociale du personnage ?\n💵 = Riche\n🧽 = Moyen\n🥻 = Pauvre`)
+                            message.edit({embeds: [embedMessage]})
+                            for (const socialClass of Role.socialClasses)
+                                message.react(socialClass)
+                            const filter = (reaction, user) => {
+                                return ['💵', '🧽', '🥻'].includes(reaction.emoji.name) && Utils.hasRights(reaction.message.guild.members.cache.get(user.id).roles.member._roles);
+                            }
+                            message.awaitReactions({ filter, max: 1, time: 120000, errors: ['time'] })
+                                .then(collected => {
+                                    const reaction = collected.first()
+                                    const socialClass = reaction.emoji.name
+                                    message.reactions.removeAll()
+                                        .catch(error => console.error('Failed to clear reactions:', error));
+                                    /** 
+                                     * CREATION DU ROLE
+                                    */
+                                    if (msg.guild.roles.cache.find(role => role.name === roleName) == undefined)
+                                    {
+                                        let rInfo = msg.guild.roles.cache.find(r => r.id === roleManager.Nb_Player[number])
+                                        // member
+                                        msg.guild.roles.create({
+                                            name: roleName,
+                                            color: roleManager.Species[species][0],
+                                            position: rInfo.rawPosition - 1,
+                                            mentionable : true,
+                                            reason: 'we needed a role for Super Cool People',
+                                        })
+                                        .then(role => {
+                                            member.roles.add(role.id);
+                                            Role.attributeRoles(member, msg, number, socialClass, species)
+                                            embedMessage = new MessageEmbed()
+                                                .setTitle('Role crée !')
+                                                .setDescription("J'ai crée et attribué les rôles du joueur o/")
+                                                .setTimestamp();
+                                    
+                                            message.edit({embeds: [embedMessage]}).then(console.log('Role Create and set with success'))
+                                        })
+                                        .catch(console.error);
+                                    }
+                                })
+                                .catch(collected => {
+                                    //message.reply('Not so classy man')
+                                })
+                        })
+                        .catch(collected => {
+                            message.reply('Number Fooo.')
+                        })
+                })
+                .catch(collected => {
+                    message.reply('You reacted with neither a thumbs up, nor a thumbs down.')
+                })
         }
-    }
+    )}
 
     // In Flood RP Channel
     if (msg.channelId == aliases.FLOOD_BOT)
