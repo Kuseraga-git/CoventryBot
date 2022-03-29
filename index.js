0ff4cc308b71653f4abe74f90f379280ee056e67
ACANTELOUP <anthony.canteloup@ynov.com>
2022-03-29 20:00:41 +0200

    i'm so dumb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 //const config = require("./config.json");
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGES] });
 client.login(process.env.TOKEN);
 /** 
  * Import other file
